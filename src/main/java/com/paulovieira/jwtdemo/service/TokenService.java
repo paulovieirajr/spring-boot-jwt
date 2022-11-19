dd5e689c23995a1e320c2bf6df0df428aac9e285
Junior Vieira <junior.vieira.1990@gmail.com>
2022-11-19 18:51:29 -0300

    :sparkles: feat: Classe de serviço para geração de token

---
+++ src/main/java/com/paulovieira/jwtdemo/service/TokenService.java
@@ -0,0 +1,36 @@
+package com.paulovieira.jwtdemo.service;
+
+import org.springframework.security.core.*;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.stereotype.*;
+
+import java.time.*;
+import java.time.temporal.*;
+import java.util.stream.*;
+
+@Service
+public class TokenService {
+
+    private final JwtEncoder encoder;
+
+    public TokenService(JwtEncoder encoder) {
+        this.encoder = encoder;
+    }
+
+    public String generateToken(Authentication authentication) {
+        var now = Instant.now();
+        var scope = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+
+        var claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(now)
+                .expiresAt(now.plus(1, ChronoUnit.HOURS))
+                .subject(authentication.getName())
+                .claim("scope", scope)
+                .build();
+
+        return this.encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+}
